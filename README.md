15068c49b6a3d8bb564a9949addd1994618fe56e
martinezparedesdavidabel-bit <martinezparedesdavidabel@gmail.com>
2026-09-20 22:40:12 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portafolio Académico · David Martínez
+
+Versión visual futurista y funcional. Abre `index.html` con Google Chrome.
+
+Las 16 semanas incluyen 32 archivos HTML de plantilla para que **Ver actividad** funcione desde el inicio. Cuando se reciban los trabajos reales, se pueden reemplazar dentro de `actividades/semana-XX/`.
---
+++ drive-config.js
@@ -0,0 +1,20 @@
+// Carpetas de evidencias en Google Drive.
+// Las dos primeras ya están configuradas con los enlaces entregados.
+const DRIVE_FOLDERS = {
+  1: 'https://drive.google.com/drive/folders/13f2vq4f4sStXGXfF_ENxVouT9nId9glN?usp=sharing',
+  2: 'https://drive.google.com/drive/folders/176pP0WSEaQwLd8Wl8nIJA_xQCK_OHUlB?usp=sharing',
+  3: 'https://drive.google.com/drive/folders/18Bdb6OoIl8V1do7Xr4bFNxg8iIjIEA3f?usp=sharing',
+  4: 'https://drive.google.com/drive/folders/1COVywA5ZChyAOGoymwl5v1jHDGffHHwJ?usp=sharing',
+  5: 'https://drive.google.com/drive/folders/1K2Zz2pLqcsH35Iv3xLGaGCc9ySGPc2De?usp=sharing',
+  6: 'https://drive.google.com/drive/folders/1KiX9wO4l7ReTvSTCMlbNAOewjUAMy5PF?usp=sharing',
+  7: 'https://drive.google.com/drive/folders/1WP93npzNt1dNJLh3mqxGoWXOU1cKDf0m?usp=sharing',
+  8: 'https://drive.google.com/drive/folders/1YwCVK92FlnI9vkB7hWF1az4ccBC65e_z?usp=sharing',
+  9: 'https://drive.google.com/drive/folders/1_jtq-MDgY9ZUHWV56Fhde7G17dp4N-o_?usp=sharing',
+  10: 'https://drive.google.com/drive/folders/1dGyLYEQcyY5qcGkWMGLhbR-ncE8T-vdF?usp=sharing',
+  11: 'https://drive.google.com/drive/folders/1eWS_wukWRY7vsvKANkF2gDsAw0m7FHBB?usp=sharing',
+  12: 'https://drive.google.com/drive/folders/1f3sUhGLi8XnxjS0fRXJM47MwdB4M9SvG?usp=sharing',
+  13: 'https://drive.google.com/drive/folders/1l0p03vYZtFKnpaw0iYltwldjX8pmgebl?usp=sharing',
+  14: 'https://drive.google.com/drive/folders/1mFZVAi5qbEjdAYfhBX7Umb9lX-MfaZnU?usp=sharing',
+  15: 'https://drive.google.com/drive/folders/1o78Y7_RRYOWuDjXBnBu88Fpij1U7nT65?usp=sharing',
+  16: 'https://drive.google.com/drive/folders/1vFgCCA-V1Vfq-T1_ut9tojXXbwAvUavp?usp=sharing'
+};
